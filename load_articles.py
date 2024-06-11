94f63eaeaba022244088b09aead9680a6e35cb64
figoyudhistiraw <figoy2@gmail.com>
2024-06-11 13:47:20 +0700

    change load article from firebase to local csv

--- load_articles.py
+++ load_articles.py
@@ -1,18 +1,36 @@
 from firebase import db
+import pandas as pd
 
 # Global variable to store articles
 ARTICLES = []
 
-# Function to fetch articles once and store in global variable
+
+# Function to fetch articles once FROM LOCAL CSV and store in global variable
 def initialize_articles():
     global ARTICLES
     try:
-        articles_ref = db.collection('articles')
-        docs = articles_ref.stream()
-        ARTICLES = [doc.to_dict() for doc in docs]
+        # Load articles from a local CSV file
+        df_articles = pd.read_csv('articles_selected_with_doi.csv')
+
+        # Convert the DataFrame to a list of dictionaries
+        ARTICLES = df_articles.to_dict(orient='records')
     except Exception as e:
         print(f"Error fetching articles: {e}")
         ARTICLES = []
 
 # Call this function when the app starts
 initialize_articles()
+
+# # Function to fetch articles once and store in global variable
+# def initialize_articles():
+#     global ARTICLES
+#     try:
+#         articles_ref = db.collection('articles')
+#         docs = articles_ref.stream()
+#         ARTICLES = [doc.to_dict() for doc in docs]
+#     except Exception as e:
+#         print(f"Error fetching articles: {e}")
+#         ARTICLES = []
+
+# # Call this function when the app starts
+# initialize_articles()
